f377ddaecb65f9765f4fb09970e8222af0d73ebb
daniel garcia <cardmaster705@gmail.com>
2017-03-14 18:30:20 -0600

    Programa Terminado

--- Practica3_metodos.java
+++ Practica3_metodos.java
@@ -9,6 +9,7 @@ int c1=0;
 int c2=0;
 int t1=0;
 int t2=0;
+int t3=0;
 String []letra={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
 
 
@@ -21,7 +22,7 @@ public void random(){
 	    	switch(randomInteger){
 	    	case 1:
 	    		if(ultima_posicion<20){
-	    		if(c1<20){
+	    		if(c1<19){
 	    		numeroBuffer();
 	    		c1=c1+1;
 	    		t1=0;
@@ -40,7 +41,7 @@ public void random(){
 	    		break;
 	    	case 2:
 	    		if(ultima_posicion<20){
-	    		if(c2<20){
+	    		if(c2<19){
 	    		letraBuffer();
 		    	c2=c2+1;
 		    	t2=0;
@@ -72,10 +73,51 @@ public void random(){
 	    		}
 	    		break;
 	    	}//fin del switch
-		}while(t1==0||t2==0);
-
+		}while(t1==0||t2==0);//termina de escribir 20 veces el proceso 1 y 2
+	do{
+		Random random = new Random();
+		int randomInteger = random.nextInt(3) + 1;
+	    System.out.println("el numero es: "+randomInteger);
+	    	switch(randomInteger){
+	    	case 1:
+	    		if(ultima_posicion>0){
+	    		t3=0;
+	    		System.out.println("Proceso terminado");
+	    		System.out.println("Posicion actual: "+ultima_posicion);
+	    		}else{
+	    			System.out.println("Buffer vacio");
+	    			t3=1;
+	    		}
+	    		break;
+	    	case 2:
+	    		if(ultima_posicion>0){
+	    		t3=0;
+	    		System.out.println("Proceso terminado");
+	    		System.out.println("Posicion actual: "+ultima_posicion);
+	    		}else{
+	    			System.out.println("Buffer vacio");
+	    			t3=1;
+	    		}
+	    		break;
+	    	case 3:
+	    		t3=0;
+	    		borraBuffer();
+	    		if(ultima_posicion>0){
+	    			ultima_posicion=ultima_posicion-1;
+	    		
+	    		System.out.println("posicion : "+ultima_posicion);	
+	    		}else{
+	    			System.out.println("Buffer vacio");
+	    			System.out.println("posicion : "+ultima_posicion);
+	    			t3=1;
+	    		}
+	    	
+	    		
+	    		break;
+	    	}//fin del switch
+		}while(t3==0);
 	
-	}
+	}//fin del metodo
 public void numeroBuffer(){
 	Random random = new Random();
 	int randomInteger = random.nextInt(9)+1;
